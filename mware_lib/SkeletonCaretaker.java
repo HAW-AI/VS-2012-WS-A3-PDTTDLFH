9285a83ec2c9257aef13a05422f6166637ab0a96
Daniel <no@reply.com>
2012-12-01 22:00:59 +0100

    implemented skeleton creation and management

---
+++ mware_lib/SkeletonCaretaker.java
@@ -0,0 +1,32 @@
+package mware_lib;
+
+import java.util.HashMap;
+import java.util.Map;
+
+import utillity.Utility;
+
+public class SkeletonCaretaker {
+	private static Map<String, Skeleton> skeletons = new HashMap<String, Skeleton>();
+
+	static void addSkeleton(Skeleton skeleton) {
+		skeletons.put(skeleton.name(), skeleton);
+	}
+	
+	public static void addSkeleton(String name, Object servant) {
+		Skeleton newSkeleton = null;
+		String type = Utility.getOriginType(servant);
+		try {
+			final Class<?>[] CONSTRUCTOR_SIGNATURE = {Class.forName("java.lang.String")/*, Class.forName(type)*/};
+			final Object[] CONSTRUCTOR_ARGS = {name/*, Class.forName(type).cast(servant)*/};
+			newSkeleton = (Skeleton) Class.forName(type+"Skeleton").getConstructor(CONSTRUCTOR_SIGNATURE).newInstance(CONSTRUCTOR_ARGS);
+			skeletons.put(name, newSkeleton);
+		} catch (Exception e) {
+			 e.printStackTrace();
+			throw new RuntimeException();
+		}
+	}
+	
+	static Skeleton getSkeleton(String name) {
+		return skeletons.get(name);
+	}
+}
