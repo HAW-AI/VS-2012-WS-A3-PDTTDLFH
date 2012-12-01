2dbd4581aae8353821139c3fc2cfe9762449a09d
Daniel <no@reply.com>
2012-12-01 20:23:21 +0100

    added methods concatStrWDel, getOriginType

--- utillity/Utility.java
+++ utillity/Utility.java
@@ -9,4 +9,24 @@ public class Utility {
         } catch (NumberFormatException ex) {}
         return result;
     }
+	
+	public static String concatStrWDel(String delimiter, String... strings) {
+	    StringBuilder sb = new StringBuilder();
+	    String sep = "";
+	    for(String s: strings) {
+	        sb.append(sep).append(s);
+	        sep = delimiter;
+	    }
+	    return sb.toString();                           
+	}
+	
+	public static String getOriginType(Object obj) {
+		String type = "java.lang.Object";
+		Class<?> objClass = obj.getClass();
+		while (objClass.getSuperclass() != null) {
+				type = objClass.getName();
+				objClass = objClass.getSuperclass();
+		}
+		return type;
+	}
 }
