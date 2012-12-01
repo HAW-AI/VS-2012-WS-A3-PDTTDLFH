f1fe6d4b7399f29fbd1607bb095d5e1e95e86062
Daniel <no@reply.com>
2012-12-01 22:26:16 +0100

    changed println text

--- mware_lib/ProxyCaretaker.java
+++ mware_lib/ProxyCaretaker.java
@@ -4,7 +4,7 @@ import java.net.InetSocketAddress;
 
 public class ProxyCaretaker {
 	static Object create(String name, String type, String host, int port) {
-		System.out.println("Creating proxy for "+name+" "+type+" "+host+" "+port);
+		System.out.println("creating proxy for "+name+" "+type+" "+host+" "+port);
 		Object result = null;
 		try {
 			final Class<?>[] CONSTRUCTOR_SIGNATURE = {Class.forName("java.lang.String"), Class.forName("java.net.InetSocketAddress")};
