8b2002bc8f01df31f316b8ec8717995288dbdc6f
Daniel <no@reply.com>
2012-12-01 20:23:38 +0100

    changed and to or

--- nameservice/Starter.java
+++ nameservice/Starter.java
@@ -4,7 +4,7 @@ import utillity.Utility;
 
 public class Starter {
 	public static void main(String[] args) {
-		if(args.length < 1 && !Utility.isInt(args[0])){
+		if(args.length < 1 || !Utility.isInt(args[0])){
 			System.out.println("Please enter the welcome socket port number as first argument");
 			System.exit(0);
 		}
