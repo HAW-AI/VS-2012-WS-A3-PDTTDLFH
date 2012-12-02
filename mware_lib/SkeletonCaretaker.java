322dd5f26daf0c999d00639c470cd216fff48dff
Daniel <no@reply.com>
2012-12-02 16:26:37 +0100

    changed skelteton.name to  getName

--- mware_lib/SkeletonCaretaker.java
+++ mware_lib/SkeletonCaretaker.java
@@ -9,15 +9,15 @@ public class SkeletonCaretaker {
 	private static Map<String, Skeleton> skeletons = new HashMap<String, Skeleton>();
 
 	static void addSkeleton(Skeleton skeleton) {
-		skeletons.put(skeleton.name(), skeleton);
+		skeletons.put(skeleton.getName(), skeleton);
 	}
 	
 	public static void addSkeleton(String name, Object servant) {
 		Skeleton newSkeleton = null;
 		String type = Utility.getOriginType(servant);
 		try {
-			final Class<?>[] CONSTRUCTOR_SIGNATURE = {Class.forName("java.lang.String")/*, Class.forName(type)*/};
-			final Object[] CONSTRUCTOR_ARGS = {name/*, Class.forName(type).cast(servant)*/};
+			final Class<?>[] CONSTRUCTOR_SIGNATURE = {Class.forName("java.lang.String"), Class.forName(type)};
+			final Object[] CONSTRUCTOR_ARGS = {name, Class.forName(type).cast(servant)};
 			newSkeleton = (Skeleton) Class.forName(type+"Skeleton").getConstructor(CONSTRUCTOR_SIGNATURE).newInstance(CONSTRUCTOR_ARGS);
 			skeletons.put(name, newSkeleton);
 		} catch (Exception e) {
